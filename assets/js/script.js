6756152b54699908841eeba6a282d6d615bf2d81
Milan Joshi <25119147+Zrepson@users.noreply.github.com>
2025-04-04 09:33:01 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "Person",
-  "name": "Milan Joshi",
-  "url": "https://milanjoshi.com.np",
-  "jobTitle": "Life Insurance Advisor and Project Coordinator based in Kathmandu, Nepal.",
-  "sameAs": [ "https://www.linkedin.com/milanjoshi18", ]
-}
-</script>
 'use strict';
 
 
