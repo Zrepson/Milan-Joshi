18a4e0bd5a724ed695064cc67386e7e3cd43fdce
Milan Joshi <25119147+Zrepson@users.noreply.github.com>
2025-06-05 17:50:05 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -501,6 +501,22 @@ deferredPrompt.userChoice.then((choiceResult) => {
   }
 });
 
+function goFullscreen() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.webkitRequestFullscreen) { /* Safari */
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) { /* IE11 */
+    document.documentElement.msRequestFullscreen();
+  }
+}
 
+document.addEventListener('fullscreenchange', () => {
+  if (document.fullscreenElement) {
+    console.log('Now in fullscreen mode');
+  } else {
+    console.log('Exited fullscreen mode');
+  }
+});
 
 
